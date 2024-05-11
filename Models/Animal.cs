d78b7ccd7d49e385c6137aeeb048e4f311bd6655
Adrianos <33091013+KrankenWargen@users.noreply.github.com>
2024-05-11 21:50:14 +0200

    Cat and dog favorite game (#3)
    * first dog cat ping pong
    * immutability
    * update scope
    * Update README.md

---
+++ Models/Animal.cs
@@ -0,0 +1,66 @@
+﻿namespace CustomDelegates.Models;
+
+public record Animal : IAnimal
+{
+    private readonly IEnumerable<Animal> _animals = new List<Animal>();
+    private SampleEventDispatcher? _dispatcher;
+
+    public Animal(IEnumerable<Animal> animals, SampleEventDispatcher? dispatcher)
+    {
+        _animals = animals;
+        _dispatcher = dispatcher;
+    }
+
+    public Animal()
+    {
+    }
+
+    public IEnumerable<int> BallCount { get; init; } = new List<int>();
+
+    private void SubscribeAnimals()
+    {
+        foreach (var animal in _animals)
+        {
+            switch (animal)
+            {
+                case Cat cat:
+                    _dispatcher += cat.Subscriber;
+                    break;
+                case Dog dog:
+                    _dispatcher += dog.Subscriber;
+                    break;
+            }
+        }
+    }
+
+    private void PassDispatcher()
+    {
+        foreach (var animal in _animals)
+        {
+            switch (animal)
+            {
+                case Cat cat:
+                    cat.setDispatcher(_dispatcher);
+                    break;
+                case Dog dog:
+                    dog.setDispatcher(_dispatcher);
+                    break;
+            }
+        }
+    }
+
+    public void Trigger()
+    {
+        if (_dispatcher is null)
+        {
+            SubscribeAnimals();
+            PassDispatcher();
+        }
+
+        _dispatcher?.Invoke(this, BallCount);
+    }
+
+    public virtual void Subscriber(object sender, IEnumerable<int> ballCount)
+    {
+    }
+}
---
+++ Models/Dog.cs
@@ -0,0 +1,29 @@
+﻿namespace CustomDelegates.Models;
+
+public record Dog : Animal
+{
+    private SampleEventDispatcher? _dispatcher;
+
+    private void TriggerUpdate(Dog newDog)
+    {
+        Console.WriteLine($"Update: {newDog.BallCount.Last()}");
+        _dispatcher?.Invoke(this, newDog.BallCount);
+    }
+
+    public override void Subscriber(object sender, IEnumerable<int> ballCount)
+    {
+        if (sender is Cat)
+        {
+            var newDog = this with
+            {
+                BallCount = ballCount.Concat(new[] { ballCount.TakeLast(2).Sum()}).ToList(),
+            };
+            TriggerUpdate(newDog);
+        }
+    }
+
+    public void setDispatcher(SampleEventDispatcher? dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+}
